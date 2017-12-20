0c516acb3734a0b9958cc0adb26b2a20eec57d1e
Duane McKibbin <duane@entelect.co.za>
2017-12-20 13:27:54 +0200

    Mobile view

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -9,76 +9,69 @@ import { DataSource } from '@angular/cdk';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Component({
-  selector: 'tulip-account ',
-  templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+    selector: 'tulip-account ',
+    templateUrl: './account.component.html',
+    styleUrls: ['./account.component.css'],
 })
 export class AccountComponent implements OnInit {
+    @Input() balances: CryptoBalance[];
+    summary: WalletSummary;
+    ethbtc: Ticker;
+    btczar: Ticker;
+    lastUpdate: string;
+    loading = true;
 
-  @Input()
-  balances: CryptoBalance[];
-  summary: WalletSummary;
-  ethbtc: Ticker;
-  btczar: Ticker;
-  lastUpdate: string;
-  loading = true;
+    displayedColumns = ['source', 'base', 'rand', 'today', 'pl'];
+    dataSource: BalanceDataSource | null;
 
-  displayedColumns = ['source', 'base', 'rand', 'today', 'pl'];
-  dataSource: BalanceDataSource | null;
+    wallets$: Observable<CryptoBalance[]>;
 
-  constructor(private dataService: DataService) {
+    constructor(private dataService: DataService) {}
+    ngOnInit() {
+        this.refresh();
+        setInterval(() => this.refresh(), 30 * 1000);
+    }
 
-  }
-  ngOnInit() {
-    this.refresh();
-    setInterval(() => this.refresh(), 30 * 1000);
-  }
+    refresh() {
+        this.loading = true;
+        this.wallets$ = this.dataService.getWallets();
+        const summary$ = this.dataService.getSummary();
 
-  refresh() {
-    this.loading = true;
-    const wallets$ = this.dataService.getWallets();
-    const summary$ = this.dataService.getSummary();
+        const table$ = Observable.forkJoin([this.wallets$, summary$]).map((results: any[]) => {
+            let wallets: CryptoBalance[];
+            let summary: WalletSummary;
 
-    const table$ = Observable.forkJoin([wallets$, summary$])
-      .map((results: any[]) => {
-        let wallets: CryptoBalance[];
-        let summary: WalletSummary;
+            [wallets, summary] = results;
 
-        [wallets, summary] = results;
+            const summaryAsWallet = new CryptoBalance();
+            summaryAsWallet.baseCurrency = 'xbt';
+            summaryAsWallet.baseValue = summary.totalBTC;
+            summaryAsWallet.currentValueRand = summary.totalRand;
+            summaryAsWallet.changeSinceStartRand = summary.gainLoss;
+            summaryAsWallet.changeSinceStartPercent = summary.gainLossPercent;
+            summaryAsWallet.changeTodayRand = summary.todayChange;
+            summaryAsWallet.changeTodayPercent = summary.todayChangePercent;
+            summaryAsWallet.source = 'Combined';
 
-        const summaryAsWallet = new CryptoBalance();
-        summaryAsWallet.baseCurrency = 'xbt';
-        summaryAsWallet.baseValue = summary.totalBTC;
-        summaryAsWallet.currentValueRand = summary.totalRand;
-        summaryAsWallet.changeSinceStartRand = summary.gainLoss;
-        summaryAsWallet.changeSinceStartPercent = summary.gainLossPercent;
-        summaryAsWallet.changeTodayRand = summary.todayChange;
-        summaryAsWallet.changeTodayPercent = summary.todayChangePercent;
-        summaryAsWallet.source = 'Combined';
+            wallets.push(summaryAsWallet);
+            this.lastUpdate = new Date().toLocaleString();
+            return wallets;
+        });
 
-        wallets.push(summaryAsWallet);
-        this.lastUpdate = new Date().toLocaleString();
-        return wallets;
-      });
-
-    this.dataSource = new BalanceDataSource(table$);
-    wallets$.subscribe();
-    summary$.subscribe();
-  }
+        this.dataSource = new BalanceDataSource(table$);
+        this.wallets$.subscribe();
+        summary$.subscribe(sum => (this.summary = sum));
+    }
 }
-
 
 export class BalanceDataSource extends DataSource<any> {
-  constructor(private balances: Observable<CryptoBalance[]>) {
-    super();
-  }
+    constructor(private balances: Observable<CryptoBalance[]>) {
+        super();
+    }
 
-  connect(): Observable<CryptoBalance[]> {
-    return this.balances;
-  }
+    connect(): Observable<CryptoBalance[]> {
+        return this.balances;
+    }
 
-  disconnect() { }
+    disconnect() {}
 }
-
-
-
--- src/app/rates/rates.component.ts
+++ src/app/rates/rates.component.ts
@@ -6,113 +6,101 @@ import { Observable } from 'rxjs/Observable';
 import * as moment from 'moment';
 
 @Component({
-  selector: 'tulip-rates',
-  templateUrl: './rates.component.html',
-  styleUrls: ['./rates.component.css']
+    selector: 'tulip-rates',
+    templateUrl: './rates.component.html',
+    styleUrls: ['./rates.component.css'],
 })
 export class RatesComponent implements OnInit {
-  @Input() balances: CryptoBalance[];
+    @Input() balances: CryptoBalance[];
 
-  lastEthbtc: Ticker;
-  lastbtczar: Ticker;
-  ethbtc: Ticker;
-  btczar: Ticker;
-  lastUpdate: string;
+    lastEthbtc: Ticker;
+    lastbtczar: Ticker;
+    ethbtc: Ticker;
+    btczar: Ticker;
+    lastUpdate: string;
 
-  loading = true;
-  zarxbtGraph: Object;
-  zarethGraph: Object;
-  zarxbtGraph2: Object;
-  zarethGraph2: Object;
+    loading = true;
+    zarxbtGraph: Object;
+    zarethGraph: Object;
+    zarxbtGraph2: Object;
+    zarethGraph2: Object;
 
-  type = 'line';
-  options = {
-    responsive: true,
-    maintainAspectRatio: false,
-    elements: {
-      point: {
-        radius: 0
-      }
-    }
-  };
-
-  constructor(private dataService: DataService) {}
-
-  ngOnInit(): void {
-    this.refresh();
-    setInterval(() => this.refresh(), 60 * 1000 * 5);
-  }
-
-  saveInstance(name, chart) {
-    setTimeout(() => {
-      chart.reflow();
-    }, 5000);
-  }
-
-  refresh() {
-    this.loading = true;
-    const bitcoin2rand$ = this.dataService.getRate('XBTZAR');
-
-    bitcoin2rand$.subscribe(btczar => {
-      this.lastbtczar = this.btczar;
-      this.btczar = btczar;
-      this.loading = false;
-      this.lastUpdate = new Date().toLocaleTimeString();
-    });
-
-    const bitcoinRate$ = this.dataService.getIntraday('XBTZAR');
-
-    bitcoinRate$.subscribe(btczar => {
-      this.zarxbtGraph = this.buildGraphOptions('ZAR/BTC Today', btczar, true);
-    });
-
-    const bitcoinDaily$ = this.dataService.getDaily('XBTZAR');
-
-    bitcoinDaily$.subscribe(btczar => {
-      this.zarxbtGraph2 = this.buildGraphOptions(
-        'ZAR/BTC Daily',
-        btczar,
-        false
-      );
-    });
-  }
-
-  private buildGraphOptions(
-    title: string,
-    tickerData: Ticker[],
-    intraday: boolean
-  ) {
-    return {
-      labels: tickerData.map(t =>
-        moment(intraday ? t.time : t.date).format(
-          intraday ? 'HH:mm' : 'YYYY-MM-DD'
-        )
-      ),
-      datasets: [
-        {
-          label: title,
-          data: tickerData.map(t => t.price)
-        }
-      ]
+    type = 'line';
+    options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        elements: {
+            point: {
+                radius: 0,
+            },
+        },
     };
-  }
 
-  private toIntradayGraphData(data: Ticker[]) {
-    return data.map(t => {
-      const x = [
-        moment(t.time)
-          .add(2, 'hours')
-          .format('HH:mm'),
-        t.price
-      ];
-      return x;
-    });
-  }
+    constructor(private dataService: DataService) {}
 
-  private toDailyGraphData(data: Ticker[]) {
-    return data.map(t => {
-      const x = [moment(t.date).valueOf(), t.price];
-      return x;
-    });
-  }
+    ngOnInit(): void {
+        this.refresh();
+        setInterval(() => this.refresh(), 60 * 1000 * 5);
+    }
+
+    saveInstance(name, chart) {
+        setTimeout(() => {
+            chart.reflow();
+        }, 5000);
+    }
+
+    refresh() {
+        this.loading = true;
+        const bitcoin2rand$ = this.dataService.getRate('XBTZAR');
+
+        bitcoin2rand$.subscribe(btczar => {
+            this.lastbtczar = this.btczar;
+            this.btczar = btczar;
+            this.loading = false;
+            this.lastUpdate = new Date().toLocaleTimeString();
+        });
+
+        const bitcoinRate$ = this.dataService.getIntraday('XBTZAR');
+
+        bitcoinRate$.subscribe(btczar => {
+            this.zarxbtGraph = this.buildGraphOptions('ZAR/BTC Today', btczar, true);
+        });
+
+        const bitcoinDaily$ = this.dataService.getDaily('XBTZAR');
+
+        bitcoinDaily$.subscribe(btczar => {
+            this.zarxbtGraph2 = this.buildGraphOptions('ZAR/BTC Daily', btczar, false);
+        });
+    }
+
+    private buildGraphOptions(title: string, tickerData: Ticker[], intraday: boolean) {
+        return {
+            labels: tickerData.map(t => moment(intraday ? t.time : t.date).format(intraday ? 'HH:mm' : 'YYYY-MM-DD')),
+            datasets: [
+                {
+                    label: title,
+                    data: tickerData.map(t => t.price),
+                },
+            ],
+        };
+    }
+
+    private toIntradayGraphData(data: Ticker[]) {
+        return data.map(t => {
+            const x = [
+                moment(t.time)
+                    .add(2, 'hours')
+                    .format('HH:mm'),
+                t.price,
+            ];
+            return x;
+        });
+    }
+
+    private toDailyGraphData(data: Ticker[]) {
+        return data.map(t => {
+            const x = [moment(t.date).valueOf(), t.price];
+            return x;
+        });
+    }
 }
